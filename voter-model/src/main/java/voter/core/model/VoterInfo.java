1892f27a3049d4a044289a708de1f5b57a2cb147
CurtisMBSmith <Curtis.MBSmith@gmail.com>
2015-02-26 00:03:25 -0400

    Added model and rest-api projects. Added projects to gradle settings. Added incomplete create tables sql script.

---
+++ voter-model/src/main/java/voter/core/model/VoterInfo.java
@@ -0,0 +1,12 @@
+package voter.core.model;
+
+import vote.core.model.Voter;
+
+public class VoterInfo extends Voter {
+
+	public VoterInfo(final String voterId) {
+		super(voterId);
+		// TODO Auto-generated constructor stub
+	}
+
+}
